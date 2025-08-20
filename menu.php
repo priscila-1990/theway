eb02396c981dd9e7faddf6db6325f1af9349b6f9
Priscila Vallejo <pvm782@gmail.com>
2025-08-20 18:20:01 -0400

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,97 @@
+.<style type="text/css">
+.Estilo1 {color: #FFFFFF}
+.Estilo2 {color: #000;
+  font-size: 20px;
+  font-family: Time New Roman;
+}
+.Estilo3 {color: #000000}
+
+.Estilo6 {font-size: 24px}
+.Estilo7 {color: #000000; font-size: 24px; }
+.Estilo11 {color: #000000; font-size: 24px; font-family: Time New Roman; font-weight: bold; }
+.Estilo13 {color: #000000; font-size: 16px;font-family: Time New Roman;}
+    </style>
+    <script>
+$(document).ready(function()
+{
+    $('[data-toggle="tooltip"]'); 
+});
+</script>
+</head>
+<body>
+<div id="cabecera">
+      <p>&nbsp;</p>
+</div>
+<div align="center" class="example">
+  <ul id="nav"><!--N-style 1287 y 1310-->
+    <li><a href="index.php"><strong>Inicio</strong></a></li>
+    <li><a href="objetivo.php"><strong>Objetivo</strong></a></li>
+    <li><a href="estructura.php"><strong>Contenido</strong></a>
+      <div>
+         <ul>   
+           <li><a href="bioquimica.php?idcontenido= 1">Bioquímica</a></li>
+           <li><a href="../bioenergetica/bioenergetica.php?idcontenido= 1">Bioenergética</a></li>
+           <li><a href="../enzimas/enzimas.php?idcontenido= 1">Enzimas</a></li>
+           <li><a href="../carbohidratos/metabolismo-C.php?idcontenido= 1">Carbohidratos</a></li>
+           <li><a href="../lipidos/metabolismo-Lp.php?idcontenido= 1">M. Lípidos</a></li>
+           <li><a href="../proteinas/metabolismo-P.php?idcontenido= 1">M. Proteinas</a></li>
+           
+         </ul>
+       </div>
+    </li>
+    <li><a href="video-bioquimica.php"><strong>Videos</strong></a>
+        <div>
+          <ul>
+           <li><a href="video-bioquimica.php">Bioquímica</a></li>
+           <li><a href="../bioenergetica/video-bioenergetica.php">Bioenergética</a></li>
+           <li><a href="../enzimas/video-enzimas.php">Enzimas</a></li>
+           <li><a href="../carbohidratos/video-MC.php">Carbohidratos</a></li>
+           <li><a href="../lipidos/video-MLp.php">M. Lípidos</a></li>
+           <li><a href="../proteinas/video-MP.php">M. Proteinas</a></li>
+          </ul>
+        </div>
+    </li>
+    <li><a href="ejerc-bioquimica.php"><strong>Autoevaluación</strong></a>
+        <div>
+          <ul>
+            <li><a href="ejerc-bioquimica.php">Bioquímica</a></li>
+            <li><a href="../bioenergetica/ejerc-bioenergetica.php">Bioenergética</a></li>
+            <li><a href="../enzimas/ejerc-enzimas.php">Enzimas</a></li>
+            <li><a href="../carbohidratos/ejerc-metabC.php">M. Carbohidratos</a></li>
+            <li><a href="../lipidos/ejerc-metabLp.php">M. Lípidos</a></li>
+            <li><a href="../proteinas/ejerc-metabP.php">M. Proteinas</a></li>
+           
+          </ul>
+        </div>
+    </li>
+    <li><a href="bibliografia.php"><strong>Bibliografía</strong></a>
+         <div>
+          <ul>
+            <li><a href="bibliografia.php">Bioquímica</a></li>
+            <li><a href="../bioenergetica/bibliografia.php">Bioenergética</a></li>
+            <li><a href="../enzimas/bibliografia.php">Enzimas</a></li>
+            <li><a href="../carbohidratos/bibliografia.php">M. Carbohidratos</a></li>
+            <li><a href="../lipidos/bibliografia.php">M. Lípidos</a></li>
+            <li><a href="../proteinas/bibliografia.php">M. Proteinas</a></li>
+           
+          </ul>
+        </div>
+    </li>
+    <li><a href="glosario.php"><strong>Glosario</strong></a>
+       <div>
+          <ul>
+            <li><a href="glosario.php">Bioquímica</a></li>
+            <li><a href="../bioenergetica/glosario.php">Bioenergética</a></li>
+            <li><a href="../enzimas/glosario.php">Enzimas</a></li>
+            <li><a href="../carbohidratos/glosario.php">M. Carbohidratos</a></li>
+            <li><a href="../lipidos/glosario.php">M. Lípidos</a></li>
+            <li><a href="../proteinas/glosario.php">M. Proteinas</a></li>
+           
+          </ul>
+        </div>
+    </li>
+    <li><a href="creditos.php"><strong>Créditos</strong></a></li>
+    <li class="pad"></li>
+  </ul>
+</div>
+<p>&nbsp;</p>
